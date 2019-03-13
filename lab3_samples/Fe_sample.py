8d24dbf6de3deee2c7385bf5bf3a54923a4b7760
Jon V <jonathan_vandermause@g.harvard.edu>
2019-03-13 15:28:05 -0400

    add antiferromagnetic example

--- lab3_samples/Fe_sample.py
+++ lab3_samples/Fe_sample.py
@@ -70,13 +70,60 @@ def compute_energy(alat, nk, ecut):
     output = parse_qe_pwscf_output(outfile=output_file)
     return output
 
+def compute_energy_anti(alat, nk, ecut):
+    """
+    Make an input template and select potential and structure, and the path where to run
+    """
+    potname = 'Fe.pbe-nd-rrkjus.UPF'
+    potpath = os.path.join(os.environ['ESPRESSO_PSEUDO'], potname)
+    pseudopots = {'Fe': PseudoPotential(path=potpath, ptype='uspp', element='Fe',
+                                        functional='GGA', name=potname),
+                  'Co': PseudoPotential(path=potpath, ptype='uspp', element='Fe',
+                                        functional='GGA', name=potname)
+                  }
+    struc = make_struc(alat=alat)
+    kpts = Kpoints(gridsize=[nk, nk, nk], option='automatic', offset=False)
+    dirname = 'Fe_a_{}_ecut_{}_nk_{}'.format(alat, ecut, nk)
+    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab3/Problem1", dirname))
+    input_params = PWscf_inparam({
+        'CONTROL': {
+            'calculation': 'scf',
+            'pseudo_dir': os.environ['ESPRESSO_PSEUDO'],
+            'outdir': runpath.path,
+            'tstress': True,
+            'tprnfor': True,
+            'disk_io': 'none',
+        },
+        'SYSTEM': {
+            'ecutwfc': ecut,
+            'ecutrho': ecut * 8,
+            'nspin': 2,
+            'starting_magnetization(1)': 1,
+            'starting_magnetization(2)': -1,
+            'occupations': 'smearing',
+            'smearing': 'mp',
+            'degauss': 0.02
+             },
+        'ELECTRONS': {
+            'diagonalization': 'david',
+            'mixing_beta': 0.5,
+            'conv_thr': 1e-7,
+        },
+        'IONS': {},
+        'CELL': {},
+        })
+
+    output_file = run_qe_pwscf(runpath=runpath, struc=struc,  pseudopots=pseudopots,
+                               params=input_params, kpoints=kpts, ncpu=2)
+    output = parse_qe_pwscf_output(outfile=output_file)
+    return output
 
 
 def lattice_scan():
     nk = 3
     ecut = 30
     alat = 3.0
-    output = compute_energy(alat=alat, ecut=ecut, nk=nk)
+    output = compute_energy_anti(alat=alat, ecut=ecut, nk=nk)
     print(output)
 
 if __name__ == '__main__':
